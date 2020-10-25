7635daf20259baf7ff6de05a1323c6f48f5174e7
David <davidmhdez22@gmail.com>
2020-10-24 22:00:47 -0500

    Fix meta viewport tag config

--- src/components/Helmets/index.js
+++ src/components/Helmets/index.js
@@ -5,7 +5,6 @@ function Helmets() {
   return (
     <Helmet>
       <title>Tennis</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"/>
     </Helmet>
   );
